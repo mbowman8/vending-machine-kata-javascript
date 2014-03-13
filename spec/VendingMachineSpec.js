c333ff1939197e8958513027de934a3b88d98684
mbowman8 <mbowman@fireproof.com>
2014-03-12 20:01:25 -0400

    buy cola

--- spec/VendingMachineSpec.js
+++ spec/VendingMachineSpec.js
@@ -1,49 +1,56 @@
 var machine;
 
-describe("Jasmine Test Runner", function() {
+describe("Jasmine Test Runner", function () {
 
-  describe("Vending Machine", function() {
+	describe("Vending Machine", function () {
 
-    beforeEach(function() {
-      machine = new Machine();
-    });
+		beforeEach(function () {
+			machine = new Machine();
+		});
 
-    afterEach(function() {
-      machine.reset();
-    });
+		afterEach(function () {
+			machine.reset();
+		});
 
-    it("can be reset", function() {
-        machine.reset();
-        expect(machine.display()).toBe("INSERT COIN");
-    });
+		it("can be reset", function () {
+			machine.reset();
+			expect(machine.display()).toBe("INSERT COIN");
+		});
 
-    it("shows a status of INSERT COIN", function() {
-        expect(machine.display()).toBe("INSERT COIN");
-    });
+		it("shows a status of INSERT COIN", function () {
+			expect(machine.display()).toBe("INSERT COIN");
+		});
 
-    it("accepts nickels", function() {
-        machine.insertNickel();
-        expect(machine.display()).toBe("5");
-    });
+		it("accepts nickels", function () {
+			machine.insertNickel();
+			expect(machine.display()).toBe("5");
+		});
 
-    it("accepts dimes", function() {
-        machine.insertDime();
-        expect(machine.display()).toBe("10");
-    });
+		it("accepts dimes", function () {
+			machine.insertDime();
+			expect(machine.display()).toBe("10");
+		});
 
-    it("accepts quarters", function() {
-        machine.insertQuarter();
-        expect(machine.display()).toBe("25");
-    });
+		it("accepts quarters", function () {
+			machine.insertQuarter();
+			expect(machine.display()).toBe("25");
+		});
 
-    it("accepts a quarter, a dime, and a nickel", function() {
-        machine.insertQuarter();
-        machine.insertDime();
-        machine.insertNickel();
-        expect(machine.display()).toBe("40");
-    });
+		it("accepts a quarter, a dime, and a nickel", function () {
+			machine.insertQuarter();
+			machine.insertDime();
+			machine.insertNickel();
+			expect(machine.display()).toBe("40");
+		});
 
-  });
+		it("rejects pennies", function () {
+			machine.insertPenny();
+			expect(machine.coinReturn()).toBe("PENNY");
+		});
 
+		it("gives cola", function () {
+			machine.buyCola();
+			expect(machine.itemContainer()).toBe("COLA");
+		});
+	});
 });
-
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,49 +1,93 @@
-var Machine = function() {
-    this.reset();
+var Machine = function () {
+	this.reset();
 };
 
-Machine.prototype.reset = function(value) {
-    that = this;
-    $(function() {
-        $(".js-output").text("INSERT COIN");
-        $(".js-insert-nickel-btn").unbind().click(function(e) {
-            that.insertNickel();
-        });
-        $(".js-insert-dime-btn").unbind().click(function(e) {
-            that.insertDime();
-        });
-        $(".js-insert-quarter-btn").unbind().click(function(e) {
-            that.insertQuarter();
-        });
-    });
-    this._value = 0;
+Machine.prototype.reset = function (value) {
+	that = this;
+	$(function () {
+		$(".js-output").text("INSERT COIN");
+		$(".js-coinReturn").text("EMPTY");
+		$(".js-itemContainer").text("EMPTY");
+
+		$(".js-insert-penny-btn").unbind().click(function (e) {
+			that.insertPenny();
+		});
+		$(".js-insert-nickel-btn").unbind().click(function (e) {
+			that.insertNickel();
+		});
+		$(".js-insert-dime-btn").unbind().click(function (e) {
+			that.insertDime();
+		});
+		$(".js-insert-quarter-btn").unbind().click(function (e) {
+			that.insertQuarter();
+		});
+		$(".js-take-penny-btn").unbind().click(function (e) {
+			that.takePenny();
+		});
+		$(".js-buy-cola-btn").unbind().click(function (e) {
+			that.buyCola();
+		});
+		$(".js-take-item-btn").unbind().click(function (e) {
+			that.takeItem();
+		});
+	 });
+	this._value = 0;
 };
 
-Machine.prototype.value = function(value) {
-    if (value === undefined) {
-        return this._value
-    } else {
-        this._value = value;
-        $(".js-output").text(value);
-    }
+Machine.prototype.value = function (value) {
+	if (value === undefined) {
+		return this._value
+	} else {
+		this._value = value;
+		$(".js-output").text(value);
+	}
 };
 
-Machine.prototype.display = function() {
-    return $(".js-output").text().trim();
+Machine.prototype.display = function () {
+	return $(".js-output").text().trim();
 };
 
-Machine.prototype.insertValue = function(value) {
-    this.value(this.value() + value);
+Machine.prototype.coinReturn = function () {
+	return $(".js-coinReturn").text().trim();
+};
+
+Machine.prototype.itemContainer = function () {
+	return $(".js-itemContainer").text().trim();
+};
+
+Machine.prototype.insertValue = function (value) {
+	this.value(this.value() + value);
 }
 
-Machine.prototype.insertNickel = function() {
-    this.insertValue(5);
+Machine.prototype.subtractValue = function (value) {
+	this.value(this.value() - value);
+}
+
+Machine.prototype.insertPenny = function () {
+	$(".js-output").text("INVALID COIN");
+	$(".js-coinReturn").text("PENNY");
 };
 
-Machine.prototype.insertDime = function() {
-    this.insertValue(10);
+Machine.prototype.insertNickel = function () {
+	this.insertValue(5);
 };
 
-Machine.prototype.insertQuarter = function() {
-    this.insertValue(25);
+Machine.prototype.insertDime = function () {
+	this.insertValue(10);
+};
+
+Machine.prototype.insertQuarter = function () {
+	this.insertValue(25);
+};
+
+Machine.prototype.takePenny = function () {
+	$(".js-coinReturn").text("EMPTY");
+};
+
+Machine.prototype.buyCola = function () {
+	$(".js-itemContainer").text("COLA");
+};
+
+Machine.prototype.takeItem = function () {
+	$(".js-itemContainer").text("EMPTY");
 };
